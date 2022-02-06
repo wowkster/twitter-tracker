2b7d74975a7a77cecaf0bd2ee8f45468e15fe989
Wowkster <adyowowk@gmail.com>
2022-02-05 23:07:57 -0500

    Fix nextauth on deploy maybe

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,25 +5,27 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
 import clientPromise from "../../../lib/mongodb"
 
-const { GITHUB_ID, GITHUB_SECRET, DISCORD_ID, DISCORD_SECRET } = process.env
+const { GITHUB_ID, GITHUB_SECRET, DISCORD_ID, DISCORD_SECRET, MONGO_URI, NEXTAUTH_URL } = process.env
 
 const options = {
     providers: [
         GithubProvider({
-            clientId: GITHUB_ID as string,
-            clientSecret: GITHUB_SECRET as string
+            clientId: GITHUB_ID!,
+            clientSecret: GITHUB_SECRET!
         }),
         DiscordProvider({
-            clientId: DISCORD_ID as string,
-            clientSecret: DISCORD_SECRET as string
+            clientId: DISCORD_ID!,
+            clientSecret: DISCORD_SECRET!
         })
     ],
     secret: process.env.SECRET,
     pages: {
         signIn: '/login',
-        newUser: '/'
+        newUser: '/',
+
     },
-    adapter: MongoDBAdapter(clientPromise),
+    database: MONGO_URI,
+    site: NEXTAUTH_URL,
 }
 
 export default (req: NextApiRequest, res: NextApiResponse<any>) => NextAuth(req, res, options)
